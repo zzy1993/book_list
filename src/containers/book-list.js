903a10b3c1e8cd79e2804782227ca13c1068127e
zzy1993 <zzy1993@163.com>
2017-01-27 10:19:06 -0500

    Add the implementation of click

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
+import { selectBook} from '../actions/index';
+import { bindActionCreators} from 'redux';
+
 
 class BookList extends Component{
   renderList() {
     return this.props.books.map((book) => {
       return (
-        <li key={book.title} className="list-group-item">
+        <li
+          key={book.title}
+          onClick={() => this.props.selectBook(book)}
+            className="list-group-item">
           {book.title}
         </li>
       );
@@ -22,10 +28,22 @@ class BookList extends Component{
     }
 }
 
+// State and data
 function mapStateToProps(state){
   return {
     books: state.books
   };
 }
 
-export default connect(mapStateToProps)(BookList);
+// Anything returned will end up as props on BookList container
+
+function mapDispatchToProps(dispatch){
+  // Whenever selectBook is called,
+  // result passed to all reducers
+  return bindActionCreators({selectBook: selectBook}, dispatch);
+}
+
+// Promote booklist from a component to a container
+// it needs to know about the new dispatch method , selectBook,
+// make available as a prop
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
